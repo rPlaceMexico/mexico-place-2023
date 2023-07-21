28817856cf721d273e2b8d30a8e0628d1164d63d
Francerz <francerzam@hotmail.com>
2023-07-20 21:41:18 -0600

    FIX element

--- userscript.user.js
+++ userscript.user.js
@@ -10,15 +10,13 @@
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
-            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
-        (function () {
+        document.querySelector('#shadow-root div.frame').appendChild((function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/francerz/mexico-place-2023/master/MexicoFTW_template.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
-        })())
-
+        })());
     }, false);
 
 }
