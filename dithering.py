4b8d68bd33f1c9c401be1af62f78b123e77ec251
Francerz <francerzam@hotmail.com>
2023-07-21 12:11:37 -0600

    CHG revert pixels mesh

--- dithering.py
+++ dithering.py
@@ -6,14 +6,14 @@ import numpy as np
 
 target_filepath = Path('MexicoFTW.png')
 img = Image.open(target_filepath).convert('RGBA')  # make sure image is RGBA
-img_3x = img.resize((img.width * 2, img.height * 2), Image.NEAREST)
+img_3x = img.resize((img.width * 3, img.height * 3), Image.NEAREST)
 img_3x_arr = np.array(img_3x)
 
 for i in range(img_3x_arr.shape[0]):
-    if i % 2 == 0:
+    if i % 3 == 0 or (i - 2) % 3 == 0:
         img_3x_arr[i, :, 3] = 0
 for i in range(img_3x_arr.shape[1]):
-    if i % 2 == 0:
+    if i % 3 == 0 or (i - 2) % 3 == 0:
         img_3x_arr[:, i, 3] = 0
 
 img_3x_back = Image.fromarray(img_3x_arr)
