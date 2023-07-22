582349b6e5a9ff2980d8a47ee49de982990fe85f
Francerz <francerzam@hotmail.com>
2023-07-21 18:56:57 -0600

    UPD userscript

--- userscript.user.js
+++ userscript.user.js
@@ -9,22 +9,25 @@
 // @grant        none
 // ==/UserScript==
 console.log('SCRIPT r/MexicoPlace template');
-
-function setImageElement() {
-    let container =
-        document.querySelector('garlic-bread-embed').shadowRoot.children[0]
-        .querySelector('garlic-bread-canvas').shadowRoot.children[0];
-    container.appendChild((function () {
-        const i = document.createElement("img");
-        i.src = "https://raw.githubusercontent.com/rPlaceMexico/mexico-place-2023/master/MexicoFTW_template.png";
-        i.style = "position:absolute;left:0;top:0;image-rendering:pixelated;width:1500px;height:1000px;";
-        console.log(i);
-        return i;
-    })());
-}
-if (window.top !== window.self) {
-    window.addEventListener('load', () => {
-        setTimeout(setImageElement, 5000);
-    }, false);
-
-}
+(function() {
+    var i = document.createElement("img");
+    var c = 0;
+    var setImageElement =function() {
+        var container = document.querySelector('garlic-bread-embed')
+            .shadowRoot.children[0].querySelector('garlic-bread-canvas')
+            .shadowRoot.children[0];
+        console.log('DET');
+        container.appendChild((function () {
+            i.src = "https://raw.githubusercontent.com/rPlaceMexico/mexico-place-2023/master/MexicoFTW_template.png?c="+c++;
+            i.style = "position:absolute;left:0;top:0;image-rendering:pixelated;width:1500px;height:1000px;";
+            setTimeout(setImageElement, 10000);
+            console.log(i);
+            return i;
+        })());
+    }
+    if (window.top !== window.self) {
+        window.addEventListener('load', () => {
+            setTimeout(setImageElement, 5000);
+        }, false);
+    }
+})();
